f780e9a1dee24daebd3c00f49a46815245de0d80
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-02-25 23:10:38 -0500

    vr
    vr

---
+++ Assets/Scripts/LHandTrack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LHandTrack : MonoBehaviour {
+
+    // Use this for initialization
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+    }
+}
---
+++ Assets/Scripts/RHandTrack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RHandTrack : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+    }
+}
+
