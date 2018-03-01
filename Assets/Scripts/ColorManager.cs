47fd397d751ea8c5f2b9e36ca28274be67653047
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-03-01 10:03:07 -0500

    drawing works

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ColorManager : MonoBehaviour
+{
+
+    public static ColorManager Instance;
+
+    public OVRInput.Button buttonA;
+   // public OVRInput.Button triggerR;
+    public Color color1;
+    public Color color2;
+    public Color color3;
+
+    //private PhotonView photonView;
+
+    void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void OnDestroy()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private Color color;
+
+    void Start()
+    {
+        color = color1;
+        //photonView = PhotonView.Get(this);
+    }
+
+    void Update()
+    {
+        if ((OVRInput.GetUp(buttonA)))
+        {
+
+           // photonView.RPC("ChangeColor", PhotonTargets.Others, null);
+
+            if (color == color1)
+            {
+                color = color2;
+            }
+            else if (color == color2)
+            {
+                color = color3;
+            }
+            else
+            {
+                color = color1;
+            }
+
+        }
+    }
+
+    public Color GetCurrentColor()
+    {
+        return this.color;
+    }
+
+    //[PunRPC]
+    public void ChangeColor()
+    {
+        if (color == color1)
+        {
+            color = color2;
+        }
+        else if (color == color2)
+        {
+            color = color3;
+        }
+        else
+        {
+            color = color1;
+        }
+    }
+}
---
+++ Assets/Scripts/DrawRight.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawRight : MonoBehaviour {
+
+    public GameObject controller;
+    public Material lineMaterial;
+    public float width;
+    public OVRInput.Button button;
+
+    private LineRenderer currLine;
+    private int numClicks = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (OVRInput.Get(button) && numClicks == 0)
+        {
+
+            GameObject stroke = new GameObject("stroke");
+
+            stroke.AddComponent<MeshFilter>();
+            stroke.AddComponent<MeshRenderer>();
+
+            currLine = stroke.AddComponent<LineRenderer>();
+            currLine.lineMaterial = new Material(lineMaterial);
+            currLine.setWidth(width);
+
+            numClicks++;
+
+
+            //photonView.RPC("AddStroke", PhotonTargets.Others, null);
+        }
+        else if (OVRInput.Get(button) && numClicks > 0)
+        {
+            currLine.AddPoint(controller.transform.position);
+
+
+            //photonView.RPC("DrawLine", PhotonTargets.Others, controller.transform.position);
+
+            numClicks++;
+        }
+        else if (OVRInput.GetUp(button))
+        {
+            numClicks = 0;
+            currLine = null;
+
+
+            //photonView.RPC("SetNull", PhotonTargets.Others, null);
+        }
+        if (currLine != null)
+        {
+            currLine.lineMaterial.color = ColorManager.Instance.GetCurrentColor();
+
+            //photonView.RPC("SetColor", PhotonTargets.Others, null);
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryThumbstickUp))
+        {
+            width = width + 0.01f;
+            //photonView.RPC("IncreaseWidth", PhotonTargets.Others, null);
+        }
+        else if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown))
+        {
+            width = width - 0.01f;
+            if (width <= 0.01f)
+            {
+                width = 0.01f;
+            }
+            //photonView.RPC("DecreaseWidth", PhotonTargets.Others, null);
+        }
+    }
+}
