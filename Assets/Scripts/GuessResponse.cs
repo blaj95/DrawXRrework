91d7e7e9bf7cf002a245c65e091ca0e109670b5b
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-25 00:12:04 -0400

    rpc to change text

---
+++ Assets/Scripts/GuessResponse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GuessResponse : Photon.MonoBehaviour {
+    public GameObject[] texts;
+    public List<Text> arResponseText;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+           texts = GameObject.FindGameObjectsWithTag("CorrectText");
+            foreach(GameObject go in texts)
+            {
+                Text text = go.GetComponent<Text>();
+                if(!arResponseText.Contains(text))
+                arResponseText.Add(text);
+                
+            }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            photonView.RPC("CorrectAnswer", PhotonTargets.All);
+        }
+        
+	}
+
+    [PunRPC]
+    public void CorrectAnswer()
+    {
+        foreach(Text te in arResponseText)
+        {
+            te.text = "You got it!";
+        }
+    }
+}
