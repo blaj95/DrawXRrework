1f92dc67c82e552721bff234b12f2819a2165204
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-26 09:59:32 -0400

    android ui scene

---
+++ Assets/Scripts/AndroidNetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AndroidNetworkManager : MonoBehaviour
+{
+    public byte Version = 1;
+
+    public virtual void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
