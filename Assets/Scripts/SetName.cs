71de7f6d05f12367e56a23d0d966c0400880da12
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-30 16:27:21 -0400

    still not working

--- Assets/Scripts/SetName.cs
+++ Assets/Scripts/SetName.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class SetName : Photon.MonoBehaviour {
     public Text _name;
     public CanvasRenderer rend;
+
+    public PhotonPlayer player { get; private set; }
+
 	// Use this for initialization
 	void Start ()
     {
@@ -19,15 +22,16 @@ public class SetName : Photon.MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-
-        _name = GameObject.FindGameObjectWithTag("playerName").GetComponent<Text>();
-        _name.text = PhotonNetwork.playerName;
-        photonView.RPC("ShowName", PhotonTargets.Others);
-        if (!photonView.isMine)
+        if (photonView.isMine)
         {
-            _name = GameObject.Find("ARPlayer(Clone)/Capsule/Canvas/Text").GetComponent<Text>();
+            _name = GameObject.FindGameObjectWithTag("playerName").GetComponent<Text>();
             _name.text = PhotonNetwork.player.NickName;
         }
+        else
+        {
+            _name.text = PhotonNetwork.player.NickName;
+        }
+        
     }
 
     [PunRPC]
